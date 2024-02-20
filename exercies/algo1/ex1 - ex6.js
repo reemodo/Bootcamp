cfd23980d235244ab680994c8e8ad32819238e6d
ReemSh <reem.shamasnah@gmail.com>
2024-02-20 15:35:26 +0200

    add algo 1

--- exercies/algo1/ex1 - ex6.js
+++ exercies/algo1/ex1 - ex6.js
@@ -35,29 +35,29 @@ printStarSeries(5, 3)
 **
 *
 Target complexity: O(mn)*/
-const paramidStart = function (num){
-	for(let i =0 ; i < num; i++){
-        let s = ""
-	for(let j = 0; j<= i; j++)
-	s+="*"
-	console.log(s)
-}
-}
-const reversParamidStar = function (num){
-	for(let i =num ; i > 0; i--){
-        let s = ""
-	for(let j = 0; j< i; j++)
-	s+="*"
-	console.log(s)
-}
-}
-const printStarSeries = function (num, count){
-	for(let i = 0; i< count;i ++){
-		paramidStart(num);
-        reversParamidStar(num-1)
-}
-}
-printStarSeries(5, 3)
+// const paramidStart = function (num){
+// 	for(let i =0 ; i < num; i++){
+//         let s = ""
+// 	for(let j = 0; j<= i; j++)
+// 	s+="*"
+// 	console.log(s)
+// }
+// }
+// const reversParamidStar = function (num){
+// 	for(let i =num ; i > 0; i--){
+//         let s = ""
+// 	for(let j = 0; j< i; j++)
+// 	s+="*"
+// 	console.log(s)
+// }
+// }
+// const printStarSeries = function (num, count){
+// 	for(let i = 0; i< count;i ++){
+// 		paramidStart(num);
+//         reversParamidStar(num-1)
+// }
+// }
+// printStarSeries(5, 3)
 /*
 Exercise 2
 
@@ -76,17 +76,17 @@ Target complexity: O(n)
 */
 
 
-const reverseString = function(str){
+// const reverseString = function(str){
         
-  let reversed = ""
-	for(let i = str.length-1 ; i >= 0;i--){
-		reversed += str.charAt(i)
-	}
-  return reversed
-}
+//   let reversed = ""
+// 	for(let i = str.length-1 ; i >= 0;i--){
+// 		reversed += str.charAt(i)
+// 	}
+//   return reversed
+// }
     
-console.log(reverseString("dog")) //should return "god"
-console.log(reverseString("race car")) //should return "rac ecar"
+// console.log(reverseString("dog")) //should return "god"
+// console.log(reverseString("race car")) //should return "rac ecar"
 
 /*
 Exercise 3
@@ -98,21 +98,21 @@ for example :
 Target complexity: O(n)
 */
 
-const findFirstNonRepeatingChar = function (str){
-	let charsMap = {}
-	for(let i =0; i < str.length; i++ ){
-		if(charsMap[str.charAt(i)])
-		    charsMap[str.charAt(i)] += 1
-		else
-			charsMap[str.charAt(i)] = 1
-	}
-	for(let i =0; i < str.length; i++ ){
-		if( charsMap[str.charAt(i)] === 1){
-			return str.charAt(i) ;
-		}
-	}
-}
-findFirstNonRepeatingChar("abca")
+// const findFirstNonRepeatingChar = function (str){
+// 	let charsMap = {}
+// 	for(let i =0; i < str.length; i++ ){
+// 		if(charsMap[str.charAt(i)])
+// 		    charsMap[str.charAt(i)] += 1
+// 		else
+// 			charsMap[str.charAt(i)] = 1
+// 	}
+// 	for(let i =0; i < str.length; i++ ){
+// 		if( charsMap[str.charAt(i)] === 1){
+// 			return str.charAt(i) ;
+// 		}
+// 	}
+// }
+// findFirstNonRepeatingChar("abca")
 
 /*
 Exercise 4
@@ -126,15 +126,15 @@ Target complexity: O(n)
 
 */
 
-function encrypt(str) {
-	let encryptedString = ""
-	for(let i = 0; i< str.length; i++){
-		encryptedString += String.fromCharCode(str.charCodeAt(i)+1)
-	}
-	return encryptedString
+// function encrypt(str) {
+// 	let encryptedString = ""
+// 	for(let i = 0; i< str.length; i++){
+// 		encryptedString += String.fromCharCode(str.charCodeAt(i)+1)
+// 	}
+// 	return encryptedString
 	
-}
-console.log(encrypt("cat"))
+// }
+// console.log(encrypt("cat"))
 
 /*
 Exercise 5
@@ -154,8 +154,8 @@ Note that the new array should have no duplicates.
 
 
 */
-const colors = ["red", "indigo", "white", "teal", "yellow"];
-const foods = ["bread", "cheese", "cucumber"];
+// const colors = ["red", "indigo", "white", "teal", "yellow"];
+// const foods = ["bread", "cheese", "cucumber"];
     
 // const jumble = function (arr1, arr2) {
 //   let jumbledArr = []
@@ -184,25 +184,108 @@ Target complexity: O(n)
 
 */
 
-function getLetter(){
-	 const rawDist = {
-        A: 60,
-        B: 10,
-        C: 10,
-        D: 20
-    };
+// function getLetter(){
+// 	 const rawDist = {
+//         A: 60,
+//         B: 10,
+//         C: 10,
+//         D: 20
+//     };
 
-    let rand = Math.random() * 100; // Generate a random number between 0 and total probability
+//     let rand = Math.random() * 100; // Generate a random number between 0 and total probability
 
-    // Iterate through the distribution object to find the appropriate letter
-    for (const letter in rawDist) {
-        rand -= rawDist[letter]; // Subtract each letter's probability from the random number
-        if (rand <= 0) {
-            return letter; // Return the letter once the random number falls within its probability range
-        }
-    }
+//     // Iterate through the distribution object to find the appropriate letter
+//     for (const letter in rawDist) {
+//         rand -= rawDist[letter]; // Subtract each letter's probability from the random number
+//         if (rand <= 0) {
+//             return letter; // Return the letter once the random number falls within its probability range
+//         }
+//     }
 
-    return 'E';
+//     return 'E';
+// }
+
+// getLetter()
+
+/*
+Exercise 7
+Remove duplicates from a linked list.
+Given the head of a singly linked list, remove duplicates from the linked list.
+Offer a way to implement a linked list in JS and use it in your solution.
+ */
+
+/**
+ * Extension 1
+Write a function called printInLoop that receives a single parameter: words - an array of strings.
+The function should print each word in words continually until you stop the program. For instance, the following code:
+printInLoop(["down", "the", "rabbit", "hole"])
+Would print "down", "the", "rabbit", "hole", then start again and print "down", "the", … until you stop your code manually (i.e. shut down your terminal if using node.js).
+ */
+// const printInLoop = function(arr){
+// 	while(true){
+// 		for(let i = 0; i< arr.length; i++)
+// 		console.log(arr[i])
+		
+// 	}
+// }
+// printInLoop(["down", "the", "rabbit", "hole"])
+
+
+/**
+ * Extension 2
+Write a function called findClosest that receives two parameters: points and point- an array of x, y coordinates, and a single x, y coordinate.
+The function should find a coordinate in points that is closest to the coordinate point. For instance:
+let pizzaLocations = [
+        { x: 6, y: 12 },
+        { x: 3, y: 1 },
+        { x: 9, y: 0 },
+        { x: 4, y: 10 }
+]
+    
+let homeLocation = { x: 4, y: 2 }
+    
+findClosest(pizzaLocations, homeLocation) // should return {x: 3, y: 1}
+You should use the Pythagorean Theorem to determine how far apart two points are.
+Target complexity: O(n)
+ */
+let pizzaLocations = [
+	{ x: 6, y: 12 },
+	{ x: 3, y: 1 },
+	{ x: 9, y: 0 },
+	{ x: 4, y: 10 }
+]
+
+let homeLocation = { x: 4, y: 2 }
+
+function findClosest (pizzaLocations, homeLocation){
+	let minDistance = Number.MAX_VALUE ;
+	let closestPoint = {};
+	for(let point of pizzaLocations ){
+		let currentDist = Math.sqrt(Math.pow(point.x- homeLocation.x,2) + Math.pow(point.y - homeLocation.y,2))
+		if(currentDist < minDistance){
+
+			minDistance = currentDist
+			closestPoint = 	point
+		}
+	}
+	return closestPoint
 }
+console.log(findClosest(pizzaLocations, homeLocation))
 
-getLetter()
+
+/**
+ * Extension 3
+Write a better cipher function called secretEncrypt
+Instead of encrypting by shifting one letter to the right, use a secret.
+For example, let’s say the word to encrypt is elephant, and the secret is cab
+The result of secretEncrypt("elephant", "cab") would be hmgsicqu
+Instead of shifting each letter one to the right, we’re always shifting by the next letter in cab:
+At first we shift the first E by c, i.e 3 letters (c is the third letter in the alphabet, the first letter of cab)
+Next we shift the L by 1 letter (a is the first letter in the alphabet, second letter of cab)
+Next we shift the second E by 2 (b is the second letter in the alphabet, third letter of cab)
+Next we shift the P by 3 again, because we’re back at c in our secret
+And so on
+Target complexity: O(n)
+Extension 4
+Naturally, write a secretDecrypt(encryptedMessage, secret) function.
+ */
